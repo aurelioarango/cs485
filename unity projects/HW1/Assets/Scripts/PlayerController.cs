d60c49333aede6748467da90ec9b74cf9d47b098
aurelioarango <arango.aurelio@hotmail.com>
2017-09-10 23:26:38 -0700

    Added Roll Ball

---
+++ unity projects/HW1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody rb;
+    public float speed;
+    public Text countText;
+    public Text result;
+    private int count;
+    void Start()
+    {
+        //looking for a rigid component 
+        rb = GetComponent<Rigidbody>();
+
+        count = 0;
+
+        SetCountText();
+        result.text = "";
+    }
+
+    void FixedUpdate()
+    {
+        //getting input from users
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertial = Input.GetAxis("Vertical");
+        //adding force
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertial);
+        rb.AddForce(movement  * speed);
+        
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Coin"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count>=12)
+        {
+            result.text = "You Win";
+        }
+    }
+}
